292eccfb26a59cddf60dd1266e1ee5acaf8807f1
julietkelson <jkelson@macalester.edu>
2019-03-15 14:12:48 -0500

    Paul's fix with the for each loop

--- prereq_dictionary.js
+++ prereq_dictionary.js
@@ -1,6 +1,7 @@
-let prereq_dict ={};
+var prereq_dict ={};
 
-for(var i = 0; i < courses.length; i++) {
-    var obj = courses[i];
-    prereq_dict[obj.dept + obj.course_num] = [obj.prereq];
+for(var course of courses) {
+    prereq_dict[course.dept + course.course_num] = [course.prereq];
 }
+
+console.log(prereq_dict);
