d9bd654f54a88d8b81dbf434e02a5798cd3ad650
kevinshin <tshin@macalester.edu>
2019-03-27 23:51:57 -0500

    Got all nodes to show.

--- VM_to_Model.js
+++ VM_to_Model.js
@@ -7,36 +7,27 @@ d3.json("./ViewModel_Test.json").then(function(data){
 function draw(ViewModel) {
 
     const radius = 35;
+    const scale = 1-(1/35);
 
     let Classes = ViewModel.Classes;
     let Connections = ViewModel.Connections;
     let svgContainer = d3.select("body")
-        .append("svg")
-        .attr("width", 1000)
-        .attr("height", 1000);
+                         .append("svg")
+                         .attr("width", 1000)
+                         .attr("height", 1000);
 
     //Circles is an array that will hold objects which represent how we want our circles
     //to be positioned. Notice that dept and course properties are directly taken from
     //the objects representing the classes.
-    let circles = [];
-    let arrows = [];
-
-    for (let object of Classes) {
-        circles.push(
-            {
-                x: Math.round(Math.random() * 100 + 40),
-                y: Math.round(Math.random() * 100 + 40),
-                dept: object.dept,
-                course: object.course
-            }
-        )
-    }
+    let taken = Classes.filter(course => course.taken === true);
+    let requiredNotTaken = Classes.filter(course => (course.taken === false && course.required === true));
+    let available = Classes.filter(course => (course.taken === false && course.required === false));
 
 
     //Make Group SVG objects, which have circles and texts. This is necessary to make
     //them respond to the same drag events.
-    let container = svgContainer.selectAll("g")
-        .data(circles)
+    let container = svgContainer.selectAll("taken")
+        .data(taken)
         .enter().append("g")
         .attr("id", function(d) {return String(d.dept) + String(d.course)})
         .call(d3.drag()
@@ -45,12 +36,8 @@ function draw(ViewModel) {
             .on("end", dragended));
 
     container.append("circle")
-        .attr("cx", function (d) {
-            return d.x;
-        })
-        .attr("cy", function (d) {
-            return d.y;
-        })
+        .attr("cx", function (d) {return d.x;})
+        .attr("cy", function (d) {return d.y;})
         .attr("r", radius)
         .style("fill", "green");
 
@@ -67,6 +54,53 @@ function draw(ViewModel) {
         .text(function (d) {
             return d.dept + d.course;
         });
+
+    let containerNotTaken = svgContainer.selectAll("requiredNotTaken")
+        .data(requiredNotTaken)
+        .enter().append("g")
+        .attr("id", function(d) {return String(d.dept) + String(d.course)})
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    containerNotTaken.append("circle")
+        .attr("cx", function (d) {return d.x;})
+        .attr("cy", function (d) {return d.y;})
+        .attr("r", radius)
+        .style("fill", "gray");
+
+    containerNotTaken.append("text")
+        .attr("x", function (d) {return d.x + 9})
+        .attr("y", function (d) {return d.y + 9})
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "18px")
+        .attr("fill", "black")
+        .text(function (d) {return d.dept + d.course;});
+
+    let containerAvailable = svgContainer.selectAll("available")
+        .data(available)
+        .enter().append("g")
+        .attr("id", function(d) {return String(d.dept) + String(d.course)})
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    containerAvailable.append("circle")
+        .attr("cx", function (d) {return d.x;})
+        .attr("cy", function (d) {return d.y;})
+        .attr("r", radius)
+        .style("fill", "red");
+
+    containerAvailable.append("text")
+        .attr("x", function (d) {return d.x + 9})
+        .attr("y", function (d) {return d.y + 9})
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "18px")
+        .attr("fill", "black")
+        .text(function (d) {return d.dept + d.course;});
+
 
     let defs = svgContainer.append('defs');
     defs.append('marker')
@@ -90,6 +124,7 @@ function draw(ViewModel) {
         .append('svg:path')
         .attr('d', 'M0,-5L10,0L0,5');
 
+    //Edges
     let paths = svgContainer.selectAll("edge")
         .data(Connections)
         .enter().append('path')
@@ -99,15 +134,18 @@ function draw(ViewModel) {
             let targetID = "#" + d.target;
             let sourceNode = d3.selectAll(sourceID)
                                .select("circle")
-                               .datum()
+                               .datum();
             let targetNode = d3.selectAll(targetID)
                                .select("circle")
-                               .datum()
+                               .datum();
             return "M" + sourceNode.x + "," + sourceNode.y + " L" +
                 targetNode.x + "," + targetNode.y;})
         .attr("stroke", "black")
         .attr("stroke-width", 4)
         .style('marker-end', 'url(#output)');
+
+    updateGraph();
+
 
     function updateGraph(){
         paths.attr('d', function(d) {
@@ -122,6 +160,7 @@ function draw(ViewModel) {
             return "M" + sourceNode.x + "," + sourceNode.y + " L" +
                 targetNode.x + "," + targetNode.y;})
     }
+
 
     function dragstarted(d) {
         d3.select(this).raise().classed("active", true);
