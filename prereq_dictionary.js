a7eea5f2dc6a9ad25a842c0e9eb9ba907a7d3f76
julietkelson <jkelson@macalester.edu>
2019-03-29 16:05:25 -0500

    updated dictionary

--- prereq_dictionary.js
+++ prereq_dictionary.js
@@ -1,7 +1,8 @@
-var prereq_dict ={};
+var prereq_dict = new Map();
 
-for(var course of courses) {
-    prereq_dict[course.dept + course.course_num] = [course.prereq];
+for (var course of courses) {
+    let courseDescription = course.dept + course.course_num;
+    prereq_dict.set(courseDescription, course.prereq);
 }
 
 console.log(prereq_dict);
