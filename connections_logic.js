5b999f67f2e7ca6eddc4cd034a431ea4033e6961
julietkelson <jkelson@macalester.edu>
2019-03-29 14:34:44 -0500

    editing connections array

--- connections_logic.js
+++ connections_logic.js
@@ -3,16 +3,16 @@
 
 let courses_stack = [];
 
-count = 0;
-let connections = function(dragged_class){
-    if (dragged_class.prereq[count] != null) {
+var count = 0;
+let connections2 = function (dragged_class) {
+    if (dragged_class.findIndex(prereq_dict.dragged_class[count]) != prereq_dict.dragged_class.length + 1 ) {
         current = prereq_dict[dragged_class][count];
         courses_stack[current] = [dragged_class];
-        connections(current);
+        connections2(current);
     }
     else {
         count++;
-        connections(dragged_class);
+        connections2(dragged_class);
     }
 }
 
@@ -20,7 +20,9 @@ var adj_list = [];
 
 //connections to adjacency list
 for (var course of connections) {
-    adj_list[i] = [connections[course], course]
+    for (var prereq of course) {
+        adj_list += [source = prereq, target = course]
+    }
 }
 
 
