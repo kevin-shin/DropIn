7bcff67947e79a5be4dbbbcb5ebd6ad5ff709fda
kevinshin <tshin@macalester.edu>
2019-03-31 14:19:44 -0500

    Kevin's structure for jest

--- JestPractice/functions.js
+++
@@ -1,5 +0,0 @@
-const functions = {
-    add: (n1,n2) => { return n1 + n2}
-}
-
-module.exports = functions;
--- JestPractice/functions.test.js
+++
@@ -1,5 +0,0 @@
-const functions = require("../functions");
-
-test("adds 2 and 2 to be 4", ()=>{
-    expect(functions.add(2,2).toBe(4));
-});
