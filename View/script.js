322904297ff4644f743c31b5d133f81588cb2ba8
kevinshin <tshin@macalester.edu>
2019-04-03 00:05:42 -0500

    Can't tell if progress or no progress

--- View/script.js
+++ View/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
         $(".draggable").draggable({
             scope: 'demoBox',
             revertDuration: 100,
@@ -9,21 +8,10 @@ $(document).ready(function () {
                 $('.result').html('-');
             }
         });
-
         $(".drag").droppable({
             scope: 'demoBox',
             drop: function (event, ui) {
-                var area = $(this).find(".area").html();
-                var box = $(ui.draggable).html();
                 $(".draggable").draggable("option", "revert", false);
-
-                //Display action in text
-                $('.result').html("[Action] <b>" + box + "</b>" +
-                    " dropped on " +
-                    "<b>" + area + "</b>");
-
-                //Realign item
-                $(ui.draggable).detach().css({top: 0, left: 0}).appendTo(this);
             }
         })
     }
