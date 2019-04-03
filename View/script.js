e2f5160e3f311ea0ea0aec009c248398a23a721b
kevinshin <tshin@macalester.edu>
2019-04-02 23:57:11 -0500

    DRAG BEHAVIOR WORKS. Terribly misformatted, but works.

--- View/script.js
+++ View/script.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-        $(".box").draggable({
-            scope: 'demoBox',
-            revertDuration: 100,
-            start: function (event, ui) {
-                //Reset
-                $(".box").draggable("option", "revert", true);
-                $('.result').html('-');
-            }
-        });
 
         $(".draggable").draggable({
             scope: 'demoBox',
@@ -19,17 +10,17 @@ $(document).ready(function () {
             }
         });
 
-        $(".drag-area").droppable({
+        $(".drag").droppable({
             scope: 'demoBox',
             drop: function (event, ui) {
                 var area = $(this).find(".area").html();
-                var box = $(ui.draggable).html()
-                $(".box").draggable("option", "revert", false);
+                var box = $(ui.draggable).html();
+                $(".draggable").draggable("option", "revert", false);
 
                 //Display action in text
                 $('.result').html("[Action] <b>" + box + "</b>" +
                     " dropped on " +
-                    "<b>" + area + "</b>")
+                    "<b>" + area + "</b>");
 
                 //Realign item
                 $(ui.draggable).detach().css({top: 0, left: 0}).appendTo(this);
