96da47eb6a42626a74220cb702c28c865465a4a9
kevinshin <tshin@macalester.edu>
2019-04-07 02:47:17 -0500

    Development of a hitTest function, eventually to be used for prereq algorithm

--- View/script.js
+++ View/script.js
@@ -4,14 +4,36 @@ $(document).ready(function() {
         courseCatalog = data;
     });
 
+    const graph = $("#graph");
+
     $(".draggable").bind( "mousedown",function() {
-        console.log($(this).id);
         var course = findCourse(courseCatalog,this);
         var description = course.courseInfo;
         var name = course.name;
         $("#courseDescription").replaceWith( "<p id='courseDescription'>" + description + "</p>" );
         $("#name").replaceWith( "<p id='name'>" + name + "</p>" );
+    })
+
+    $(".draggable").bind( "mouseup",function() {
+            if (hitTest(this,graph)){
+                alert("hittest!")
+            }
     });
+
+    function hitTest(object,bounds){
+        var course = object.getBoundingClientRect();
+        var graph = bounds.getBoundingClientRect();
+        var center = {
+            x: (course.left+course.right)/2,
+            y: (course.top+course.bottom)/2
+        };
+        if (graph.left < center.x && center.x < graph.right){
+            if (graph.top < center.y && center.y < graph.bottom ){
+                alert("Hit Test! \n Object " + object + " collided with " + bounds)
+            }
+        }
+    }
+
 
     function findCourse(data,course) {
         let ID = course.id;
