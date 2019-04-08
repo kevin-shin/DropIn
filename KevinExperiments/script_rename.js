486e459e99963f6c6e0b600bc70e81338bddad5a
kevinshin <tshin@macalester.edu>
2019-04-08 18:22:06 -0500

    need to go back to a previous commit, accidentally deleted a function.

--- KevinExperiments/script_rename.js
+++ KevinExperiments/script_rename.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
     const radius = 20;
+    const displacement = radius + 10;
 
     var courseCatalog, viewModel, connections;
-    d3.json("../Model/CS_major.json").then(function (data) {
+    d3.json("./ViewModel_Test.json").then(function (data) {
         courseCatalog = data;
         viewModel = data;
         connections = data.Connections;
@@ -11,33 +12,7 @@ $(document).ready(function () {
 
     $(function () {
         var availableCourses = $(".draggable");
-
-
-        // //Make element draggable
-        // var instance = jsPlumb.getInstance({
-        //     Connector: ["Straight"],
-        //     DragOptions: {cursor: "pointer", zIndex: 5},
-        //     PaintStyle: {stroke: "black", strokeWidth: 2},
-        // });
-        //
-        // instance.draggable(availableCourses);
-        //
-        // function initializeConnections() {
-        //     for (let connection of connections) {
-        //         instance.connect({
-        //             source: connection.source,
-        //             target: connection.target,
-        //             endpoint: "Blank",
-        //             anchors: [
-        //                 ["Perimeter", {shape: "Diamond", anchorCount: 150}],
-        //                 ["Perimeter", {shape: "Diamond", anchorCount: 150}]
-        //             ]
-        //         })
-        //     }
-        // }
-
-        availableCourses.draggable({
-        });
+        availableCourses.draggable({});
 
         $("#svgNotTaken").droppable({
             accept: '.draggable',
@@ -47,15 +22,45 @@ $(document).ready(function () {
             drop: function (e, ui) {
                 var x = ui.helper.clone();
                 ui.helper.remove();
-                x.css({top: e.clientY - radius, left: e.clientX - radius, position:'absolute'});
+                x.css({top: e.clientY - displacement, left: e.clientX - displacement, position: 'absolute'});
                 $("#graph").append(x);
                 $(".draggable").draggable();
 
             }
         });
+        //Make element draggable
+        var instance = jsPlumb.getInstance({
+            Connector: ["Straight"],
+            DragOptions: {cursor: "pointer", zIndex: 5},
+            PaintStyle: {stroke: "black", strokeWidth: 2},
+        });
 
-        // initializeConnections();
-        // jsPlumb.fire("jsPlumbDemoLoaded", instance);
+        function initializeConnections() {
+            for (let connection of connections) {
+                instance.connect({
+                    source: connection.source,
+                    target: connection.target,
+                    endpoint: "Blank",
+                    anchors: [
+                        ["Perimeter", {shape: "Diamond", anchorCount: 150}],
+                        ["Perimeter", {shape: "Diamond", anchorCount: 150}]
+                    ]
+                })
+            }
+        }
+
+        $(".draggable").bind("mousedown", function () {
+            var course = findCourse(courseCatalog, this);
+            var description = course.courseInfo;
+            var name = course.name;
+            $("#courseDescription").replaceWith("<p id='courseDescription'>" + description + "</p>");
+            $("#name").replaceWith("<p id='name'>" + name + "</p>");
+        });
+
+
+        initializeConnections();
+        jsPlumb.fire("jsPlumbDemoLoaded", instance);
+
 
 
     });
@@ -84,19 +89,4 @@ $(document).ready(function () {
         // jsPlumb.draggable($(".jsPlumbItem"));
 
     }
-
-    //
-    // $(".draggable").bind( "mousedown",function() {
-    //     var course = findCourse(courseCatalog,this);
-    //     var description = course.courseInfo;
-    //     var name = course.name;
-    //     $("#courseDescription").replaceWith( "<p id='courseDescription'>" + description + "</p>" );
-    //     $("#name").replaceWith( "<p id='name'>" + name + "</p>" );
-    // })
-    //
-    // $(".draggable").bind( "mouseup",function() {
-    //         if (hitTest(this,graph)){
-    //             alert("hittest!")
-    //         }
-    // });
 });
