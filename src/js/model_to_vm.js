fe1a905669ea3c51de64e93d7ffb5ba9b57f2d3d
kevinshin <tshin@macalester.edu>
2019-04-19 18:13:22 -0500

    some updated cleaning things

--- src/js/model_to_vm.js
+++ src/js/model_to_vm.js
@@ -3,86 +3,6 @@ import { makeConnections, dfs } from "./connections_logic.js";
 // const dfs = require('./connections_logic');
 // const makeConnections = require("./connections_logic");
 
-//initialize VM. input profile and major logic output viewmodel.
-let initializeVM = function(profile, rules){
-    let VM = [];
-
-    //Intro courses
-    for (let course of rules.intro.courses){
-        if (profile.some((element) => course === element)){
-            VM.push({
-                course: course,
-                taken: true,
-                required: true
-            })
-        }
-        else {
-            VM.push({
-                course: course,
-                taken: false,
-                required: true
-            })
-        }
-    }
-
-    //Core courses
-    for (let course of rules.core.courses){
-        if (profile.some((element) => course === element)){
-            VM.push({
-                course: course,
-                taken: true,
-                required: true
-            })
-        }
-        else {
-            VM.push({
-                course: course,
-                taken: false,
-                required: true
-            })
-        }
-    }
-
-    //Math courses
-    for (let course of rules.math.courses) {
-        if (profile.some((element) => course === element)){
-            VM.push({
-                course: course,
-                taken: true,
-                required: false
-            })
-        }
-        else {
-            VM.push({
-                course: course,
-                taken: false,
-                required: false
-            })
-        }
-    }
-
-    for (let course of rules.elective.courses){
-        if (profile.some((element) => course === element)){
-            VM.push({
-                course: course,
-                taken: true,
-                required: false
-            })
-        }
-        else {
-            VM.push({
-                course: course,
-                taken: false,
-                required: false
-            })
-        }
-    }
-    return VM;
-};
-
-
-
-
 /*  
  * Adds a course and its prereqs to the profile to be read by function writeSourceTarget for VM
  * Takes a profile and the course ("COMP225") that was just dragged onto the screen
@@ -118,18 +38,18 @@ let deleteCourseProfile = function (profile, deletedCourse) {
 let writeSourceTarget = function (profile) {
     var connections = [];
     for (let node of profile) {
-        makeConnections(node.course);
-        if (!connections.some((conn) => conn.source === prereq && conn.target === course)) {
-            connections.push({
-                "source": prereq,
-                "target": course
-            });
-        }
+        console.log(makeConnections(node.course));
+        // if (!connections.some((conn) => conn.source === prereq && conn.target === course)) {
+        //     connections.push({
+        //         "source": prereq,
+        //         "target": course
+        //     });
+        // }
     }
    return connections;
 }
 
-export { makeConnections, writeSourceTarget, updateProfile, deleteCourseProfile };
+export { writeSourceTarget, updateProfile, deleteCourseProfile };
     
 // module.exports = {};
 // module.exports.updateProfile = updateProfile;
