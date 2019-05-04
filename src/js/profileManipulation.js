07f430af5d804ec5fb64e31ae8924cc270539f2d
julietkelson <jkelson@macalester.edu>
2019-05-04 13:09:05 -0500

    randomized prerequisite location with reasonable logic

--- src/js/profileManipulation.js
+++ src/js/profileManipulation.js
@@ -11,6 +11,109 @@ let makeProfile = function(inputArray){
     return Profile;
 };
 
+/*
+* generates random int to be used in updating position
+*/
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/*
+ *updates y position for auto added prereqisites
+ * @param initPos: initial y position of dragged course
+ */
+function updatePosy(initPos){
+    if (initPos < 30){
+        return initPos;
+    }
+    else if(initPos < 100 ) {
+        var newPos = initPos - randomInt(35, 60);
+        while (newPos < 0) {
+            newPos = initPos - randomInt(35, 60);
+        }
+        return newPos;
+    }
+    else if(initPos < 200) {
+        newPos = initPos - randomInt(60, 90);
+        while (newPos < 0) {
+            newPos = initPos - randomInt(60, 90);
+        }
+        return newPos;
+    }
+    else if(initPos < 400) {
+        newPos = initPos - randomInt(60, 200);
+        while (newPos < 0) {
+            newPos = initPos - randomInt(60, 2000);
+        }
+        return newPos;
+    }
+    else if(initPos < 1000) {
+        newPos = initPos - randomInt(60, 200);
+        while (newPos < 0) {
+            newPos = initPos - randomInt(60, 200);
+        }
+        return newPos;
+    }
+}
+
+/*
+* randomly decides addition or subtraction
+* to be used when generating position of prerequisites
+*/
+function plusOrMinus(x, y){
+    let decide = randomInt(0,1);
+    if(decide === 1){
+        return x + y;
+    }
+    else{
+        return x - y;
+    }
+}
+
+/*
+ *updates x position for auto added prereqisites
+ * @param initPos: initial x position of dragged course
+ */
+function updatePosx(initPos){
+    if(initPos <30 || initPos > 590){ //if the initial class is at an edge
+        if(initPos > 590 ){
+            return initPos - randomInt(20, 50);
+        }
+        else {
+            return initPos;
+        }
+    }
+    else if(initPos < 100 ) {
+        var newPos = plusOrMinus(initPos,randomInt(30, 50));
+        while (newPos < 0 || newPos > 580) {
+            newPos = plusOrMinus(initPos, randomInt(30, 50));
+        }
+        return newPos;
+    }
+    else if(initPos < 200) {
+        newPos = plusOrMinus(initPos, randomInt(0, 90));
+        while (newPos < 0 || newPos > 580) {
+            newPos = plusOrMinus(initPos, randomInt(60, 90));
+        }
+        return newPos;
+    }
+    else if(initPos < 400) {
+        newPos = plusOrMinus(initPos , randomInt(0, 200));
+        while (newPos < 0 || newPos > 580) {
+            newPos = plusOrMinus(initPos , randomInt(60, 2000));
+        }
+        return newPos;
+    }
+    else if(initPos < 1000) {
+        newPos = plusOrMinus(initPos , randomInt(0, 200));
+        while (newPos < 0 || newPos > 580) {
+            newPos = plusOrMinus(initPos , randomInt(0, 200));
+        }
+        return newPos;
+    }
+}
+
+
 let addCourseToProfile = function (profile, draggedCourse, eventX, eventY) {
 
     let courseWithPrereqs = dfs(draggedCourse);
@@ -24,8 +127,8 @@ let addCourseToProfile = function (profile, draggedCourse, eventX, eventY) {
                 x: initialPositionX,
                 y: initialPositionY
             });
-            initialPositionX -= 100;
-            initialPositionY -= 100;
+            initialPositionX = updatePosx(initialPositionX);
+            initialPositionY = updatePosy(initialPositionY);
         }
     }
 };
