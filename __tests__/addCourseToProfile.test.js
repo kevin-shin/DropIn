5edadcb04c80480c40c3a1ce243113bba12009f0
julietkelson <jkelson@macalester.edu>
2019-05-10 23:38:26 -0500

    addCourseToProfile tests

--- __tests__/addCourseToProfile.test.js
+++ __tests__/addCourseToProfile.test.js
@@ -1,4 +1,4 @@
-const addCourseToProfile = require("../src/js/profileManipulation.js");
+const profManipModule = require("../src/js/profileManipulation.js");
 
 let beginnerProfile = [
     {
@@ -21,8 +21,10 @@ let beginnerProfile = [
     }
 ];
 
-test('beginner profile add comp128', () =>{
-    expect(addCourseToProfile(beginnerProfile, "COMP128", 382, 583)).toMatchObject(
+profManipModule.addCourseToProfile(beginnerProfile, "COMP225", 327, 391);
+
+test('add course with no prereq', () => {
+    expect(beginnerProfile).toMatchObject(
         [
             {
                 course: "COMP123",
@@ -43,11 +45,169 @@ test('beginner profile add comp128', () =>{
                 y: 274
             },
             {
-                course: "COMP128",
+                course: "COMP225",
                 status: "planned",
-                x:382,
-                y:583
+                x: 327,
+                y: 391
             }
         ]
     )
+
+});
+
+let planningProfile = [
+    {
+        course: "COMP123",
+        status: "planned",
+        x: 127,
+        y: 384
+    },
+    {
+        course: "COMP127",
+        status: "planned",
+        x: 274,
+        y: 480
+    },
+    {
+        course: "MATH279",
+        status: "planned",
+        x: 958,
+        y: 274
+    }
+];
+
+profManipModule.addCourseToProfile(planningProfile, "COMP240", 327, 391);
+
+test('add course with  prereqs', () => {
+    expect(planningProfile).toMatchObject(
+            [{
+                "course": "COMP123",
+                "status": "planned",
+                "x": 127,
+                "y": 384
+            },
+            {
+                "course": "COMP127",
+                "status": "planned",
+                "x": 274,
+                "y": 480
+            },   
+            {
+                "course": "MATH279",
+                "status": "planned",
+                "x": 958,
+                "y": 274
+            },
+            {
+                course: "COMP240",
+                status: "planned",
+                x: expect.any(Number),
+                y: expect.any(Number)
+            },
+            {
+                course: "COMP128",
+                status: "planned",
+                x: expect.any(Number),
+                y: expect.any(Number)
+            }
+        ]
+    )
+
+});
+
+let intermediateProfile = [
+    {
+        course: "COMP123",
+        status: "taken",
+        x: 127,
+        y: 384
+    },
+    {
+        course: "COMP127",
+        status: "taken",
+        x: 274,
+        y: 480
+    },
+    {
+        course: "MATH279",
+        status: "taken",
+        x: 958,
+        y: 274
+    },
+    {
+        course: "COMP128",
+        status: "taken",
+        x: 264,
+        y: 482
+    },
+    {
+        course: "COMP225",
+        status: "planned",
+        x: 463,
+        y: 736
+    },
+    {
+        course: "COMP240",
+        status: "planned",
+        x: 463,
+        y: 856
+    }
+];
+
+profManipModule.addCourseToProfile(intermediateProfile, "MATH253", 327, 391);
+
+test('profile with many courses, add course with prerequisites', () => {
+    expect(intermediateProfile).toMatchObject(
+        [
+            {
+                course: "COMP123",
+                status: "taken",
+                x: 127,
+                y: 384
+            },
+            {
+                course: "COMP127",
+                status: "taken",
+                x: 274,
+                y: 480
+            },
+            {
+                course: "MATH279",
+                status: "taken",
+                x: 958,
+                y: 274
+            },
+            {
+                course: "COMP128",
+                status: "taken",
+                x: 264,
+                y: 482
+            },
+            {
+                course: "COMP225",
+                status: "planned",
+                x: 463,
+                y: 736
+            },
+            {
+                course: "COMP240",
+                status: "planned",
+                x: 463,
+                y: 856
+            },
+            {
+                course: "MATH253",
+                status: "planned",
+                x: 327,
+                y:391
+            },
+            {
+                course: "MATH155",
+                status: "planned",
+                x: expect.any(Number),
+                y: expect.any(Number)
+            }
+        ]
+    )
+
 });
